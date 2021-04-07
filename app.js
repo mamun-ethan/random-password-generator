f5e88b454c2ed265ecf5836742b8bc5232d691a0
Mamun sikder <mamunsikder73375@gmail.com>
2021-04-07 23:31:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+ const pass_keys = [
+     "A",
+     "B",
+     "C",
+     "D",
+     "E",
+     "F",
+     "G",
+     "H",
+     "I",
+     "J",
+     "K",
+     "L",
+     "M",
+     "N",
+     "O",
+     "P",
+     "Q",
+     "R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",0,1,2,3,4,5,6,7,8,9,
+	 "~",
+	 "!",
+	 "@",
+	 "#",
+	 "$",
+	 "%",
+	 "^",
+	 "&","*","(",")","-","_","=","+","<",">","?","/","|",":"
+
+ ];
+ const button = document.querySelector('button')
+
+ 
+
+  button.addEventListener('click',function(){
+      let temp_pass = ""
+      for(let i = 0;i<16;i++){
+          const index = parseInt(Math.random() * pass_keys.length)
+         temp_pass += pass_keys[index]
+        document.getElementById('password').value = temp_pass
+     
+      }
+  })
